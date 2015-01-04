83b6c163b8a806f99766acf80e062925e57c784e
kero3 <kero3@outlook.jp>
2015-01-04 18:12:47 +0900

    かけざんテスト

--- かけざんテスト/Timer.cs
+++ かけざんテスト/Timer.cs
@@ -8,16 +8,22 @@ using System.Diagnostics;
 
 namespace かけざんテスト
 {
+    /// <summary>
+    /// カウントダウンタイマー(分のみ)
+    /// </summary>
+    /// 指定時間(3分など)を受け取り、その時間のミリ秒をメモリに格納。
+    /// ストップウォッチの値からその時間を引いた値を表示する。
     class Timer : INotifyPropertyChanged
     {
+        private long setTime;
         public string NowTime { get; private set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
         Stopwatch wc = new Stopwatch();
 
-        public Timer()
+        public Timer(int t)
         {
-
+            setTime = t * 60 * 1000;
             Run();
         }
 
@@ -46,7 +52,9 @@ namespace かけざんテスト
             {
                 await Task.Delay(10);
 
-                TimeSpan ts = wc.Elapsed;
+                long time = setTime - wc.ElapsedMilliseconds;
+                TimeSpan ts = new TimeSpan(time*1000*10);
+                //TimeSpan ts = wc.Elapsed;
 
                 // Format and display the TimeSpan value.
                 NowTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
@@ -55,8 +63,8 @@ namespace かけざんテスト
                 if (this.PropertyChanged != null)
                 {
                     this.PropertyChanged(this, new PropertyChangedEventArgs("NowTime"));
-                    //指定時間を過ぎたことを通知する(1分)
-                    if (ts.Minutes >= 2)
+                    //指定時間を過ぎたことを通知する
+                    if (time <= 0)
                     {
                         System.Windows.MessageBox.Show("おしまい");
                         reset();
